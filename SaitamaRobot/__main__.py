cee904158fa54edf9dfdcdd6b55674915a2606ad
Bitcoinblack482 <80901864+Bitcoinblack482@users.noreply.github.com>
2022-02-25 13:21:20 +0800

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -82,7 +82,6 @@ Hey there! My name is *{}*.
 *Main* commands available:
  • /help: PM's you this message.
  • /help <module name>: PM's you info about that module.
- • /donate: information on how to donate!
  • /settings:
    • in PM: will send you your settings for all supported modules.
    • in a group: will redirect you to pm, with all that chat's settings.
