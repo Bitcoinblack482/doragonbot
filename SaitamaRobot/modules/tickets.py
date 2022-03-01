40f3fe4494e8dc55c2cce88a29a5ef2f40b14120
Bitcoinblack482 <80901864+Bitcoinblack482@users.noreply.github.com>
2022-03-01 15:36:58 +0800

    Update tickets.py

--- SaitamaRobot/modules/tickets.py
+++ SaitamaRobot/modules/tickets.py
@@ -1,1 +1,56 @@
+import requests
+from SaitamaRobot import CASH_API_KEY, dispatcher
+from telegram import Update, ParseMode
+from telegram.ext import CallbackContext, CommandHandler, run_async
+import logging
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import CommandHandler, InlineQueryHandler, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
+from telegram.ext import Updater, CallbackQueryHandler, CallbackContext
+import random
+import time
 
+
+ONE , TWO , THREE, FOUR , FIVE, *_ = range(1000)
+
+def ticket(update, context):
+    cd = context.chat_data
+    query = update.callback_query
+    user = update.effective_user.name
+    cd['id'] = update.effective_user.id
+    context.bot.send_message(chat_id = effective_chat.id, text = "<b>Please send your questions or inquiry in the next message</b>\n\n<i>Admins will get back to you very soon</i>", parse_mode = ParseMode.HTML)
+    return TWO
+  
+def ticket2(update , context):
+    query = update.callback_query
+    cd = context.chat_data
+    inquiry = update.message.text
+    context.bot.forward_message(chat_id = -753748989  , text = inquiry)
+    return ONE
+  
+def ticket3(update , context):
+    cd = context.chat_data
+    query = update.callback_query
+    answer = update.message.reply_text
+    id = cd['id']
+    context.bot.send_message(chat_id = id, text = answer)
+    ConversationHandler.END
+    
+ticket_handler = ConversationHandler(
+    entry_points=[CommandHandler('ticket', ticket)],
+    states={
+        ONE:
+            [
+                CallbackQueryHandler(ticket2, pattern=".")
+            ],
+        TWO:
+            [
+                MessageHandler(Filters.text, ticket)
+            ]
+      
+    },
+    fallbacks=[]
+)
+
+
+dispatcher.add_handler(ticket_handler)
