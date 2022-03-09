c5128ef6ce01c41c24ac9ce910ed6990eef77daa
Bitcoinblack482 <80901864+Bitcoinblack482@users.noreply.github.com>
2022-03-09 17:26:33 +0800

    Update dor.py

--- SaitamaRobot/modules/dor.py
+++ SaitamaRobot/modules/dor.py
@@ -16,15 +16,30 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Upda
 cg = CoinGeckoAPI()
 b = cg.get_coins_list()
 
-dor = cg.get_price(ids='doragonland', vs_currencies='usd',include_market_cap='true', include_24hr_vol='true', include_24hr_change='true')
 
-aa = dor['doragonland']['usd']
-ab = round(dor['doragonland']['usd_24h_vol'],2)
-ac = round(dor['doragonland']['usd_24h_change'],2)
+
+
 #ad = dor['doragonland']['usd']
 
-tt = f'Name : DOR \nPRICE : {aa}$\n24 hour volume :{ab}$\n24 hour percent change : {ac}%'
+
 def dor(update , context):
+    dor = cg.get_price(ids='doragonland', vs_currencies='usd',include_market_cap='true', include_24hr_vol='true', include_24hr_change='true')
+    aa = dor['doragonland']['usd']
+    ab = round(dor['doragonland']['usd_24h_vol'],2)
+    ac = round(dor['doragonland']['usd_24h_change'],2)
+
+    if ac <0:
+        ad = '😢'
+    if ac>5 and ac<=10:
+        ad ='😃'
+    if ac>10 and ac<=30:
+        ad ='🎊'
+    if ac>30:
+        ad ='🌙'
+    
+    
+    tt = f'<b>Name :</b> DOR \n\n<b>Price :</b> {aa}$\n<b>24 hour volume :</b>{ab}$\n<b>24 hour percent change :</b> {ac}% {ad}'
+    
     context.bot.send_message(chat_id = update.effective_chat.id, text =tt, parse_mode = ParseMode.HTML )
     
     
