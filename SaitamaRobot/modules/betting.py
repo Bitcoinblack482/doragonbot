746cf769776c5ae79d175755e19c91131e412cd0
Bitcoinblack482 <80901864+Bitcoinblack482@users.noreply.github.com>
2022-07-06 01:05:06 +0800

    Update betting.py

--- SaitamaRobot/modules/betting.py
+++ SaitamaRobot/modules/betting.py
@@ -14,27 +14,166 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Upda
 ONE , TWO , THREE , FOUR , FIRST , SECOND,  *_ = range(50)
 
 boss = [163494588, 1610096351]
-c1 = []
-c2 = []
+c = []
+list = []
+fees1 = 0
+fees2 = 0
+total = 0
+share1 = 0
+share2 = 0
 
 def startbet(update , context):
-  c1 = update.message.text.split()[1]
-  c2 = update.message.text.split()[2]
-  
-  
+  name = update.effective_user.name
+  id = update.effective_user.id
+  try:
+   c1 = update.message.text.split()[1]
+   c2 = update.message.text.split()[2]
+  except IndexError:
+    update.message.reply_text('Please provide 2 users to compete')
+    return -1
+
+  if id not in boss:
+        update.message.reply_text('Not authorised')
+        return -1
+    
+  c.append(c1)
+  c.append(c2)
+    
+def betboard(update , context):
+    global list
+    global c
+    n = 0
+    m = 0
+    text1 = ''
+    text2 = ''
+    
+    fees1 = total*0.02
+    fees2 = total*0.15
+    shares = total*0.83
+    
+    for i in list:
+        
+    if len(c)==0:
+        update.message.reply_text('No competition going on right now')
+        return -1
+    
+    update.message.reply_text(f'<b>Ongoing competitions\n\n</b>'
+                              f'<b>c[0]</b>\nVs.'
+                              f'<b>c[1]</b>\n\n'
+                              f'Total pot = {total}\n'
+                              f'Middle man fees = {fees1}\n'
+                              f'Winner pot = {fees2}\n'
+                              f'Total pot for shares = {shares}\n', parse_mode = ParseMode.HTML)
+    
+    
+def bet(update , context):
+    cd = context.chat_data
+    query = update.callback_query
+    cd['name'] = name = update.effective_user.name
+    id = update.effective_user.id
+    
+    if len(c)==0:
+        update.message.reply_text('No competition going on right now')
+        return -1
+    
+    try:
+    
+     cd['bet_on'] = bet_on = update.message.text.split()[1]
+     if bet_on not in c:
+        update.message.reply_text(f'currently you can bet on {c[0]}, or {c[1]}, write either one, correct spelling')
+        return -1
+     cd['amount'] = amount = update.message.text.split()[2]
+    except IndexError:
+     update.message.reply_text(f'use it like this /bet <bet on who> <amount>\n\n'
+                              f'currently you can bet on {c[0]}, or {c[1]}')
+     return -1
+    
+    keyboard = [[InlineKeyboardButton('Approve', callback_data='approve')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    update.message.reply_text(f'You bet on {bet_on} with {amount} DOR\n\n <b>waiting for admin to check and approve</b>', parse_mode=ParseMode.HTML,
+                            reply_markup=reply_markup)
+    
+def bet2(update , context):
+    global list
+    cd = context.chat_data
+    query = update.callback_query
+    
+    bet_on = cd['bet_on']
+    amount = cd['amount']
+    name  = cd['name']
+    
+    
+    if update.callback_query.from_user.id not in boss:
+        query.answer('Not authroised')
+        return None
+    
+    total += amount
+    d = {'user':name, 'bet_on':bet_on, 'amount':amount}
+    list.append(d)
+    
+    query.edit_message_text(f'<b>Your bet on {bet_on} with {amount} DOR has been approved</b>', parse_mode=ParseMode.HTML)
+    return ConversationHandler.END
+
+
+def mybet(update , context):
+    global share1
+    global share2
+    cd = context.chat_data
+    query = update.callback_query
+    name = update.effective_user.name
+    id = update.effective_user.id
+    ratio = 0
+    reward = 0
+    
+    for i in list:
+     if i['bet_on'] == c[0]:
+      share1 += i['amount']
+     if i['bet_on'] == c[1]:
+      share2 += i['amount']
+    
+    for i in list:
+     if i['user'] == name:
+      if i['bet_on'] == c[0]:
+       ratio += i['amount']/share1
+      if i['bet_on'] == c[1]:
+       ratio += i['amount']/share2
+    
+    for i in list:
+     if i['user'] == name:
+      if i['bet_on'] == c[0]:
+        reward +=share1*ratio
+     if i['user'] == name:
+      if i['bet_on'] == c[1]:
+        reward +=share2*ratio
+    
+    
+    text = ''
+    
+    for i in list:
+        if i['user'] == name:
+            text+= f'{name}\nYour bet amount :{i['amount']}\nYou bet on {i["bet_on"]}\n\nExpected reward if i["bet_on"] win : {reward} DOR'
+     
+    update.message.reply_text(text)
+            
+      
+    
     
 
+dispatcher.add_handler(CommandHandler("startbet", startbet))
+BET_HANDLER = ConversationHandler(
+        entry_points=[CommandHandler('bet', bet, pass_user_data=True)],
+        states={
+            ONE: [CallbackQueryHandler(bet2, pattern="approve", pass_user_data=True),]
+        },
+        fallbacks=[],
+        allow_reentry=True,
+        per_user=False,
+        run_async=True
+    )
 
-
-
-
-
-
-
-
-
-
-
-START_HANDLER = CommandHandler('startbet', startbet)
+dispatcher.add_handler(BET_HANDLER)
+dispatcher.add_handler(CommandHandler("mybet", mybet))
+dispatcher.add_handler(CommandHandler("betboard", betboard))
 
 
