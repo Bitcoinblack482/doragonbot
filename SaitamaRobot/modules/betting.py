a426d36267ee40781b8605f688855380c2fa695f
Bitcoinblack482 <80901864+Bitcoinblack482@users.noreply.github.com>
2022-07-06 14:52:14 +0800

    Update betting.py

--- SaitamaRobot/modules/betting.py
+++ SaitamaRobot/modules/betting.py
@@ -200,9 +200,11 @@ def mybet(update , context):
     for i in list:
         if i['user'] == name:
             text+= f'{name}\nYour bet amount :{i["amount"]}\nYou bet on {i["bet_on"]}\n\nExpected reward if i["bet_on"] win : {reward} DOR'
-     
-    update.message.reply_text(text)
-            
+    if text == '' 
+     update.message.reply_text('You dont have ongoing bet yet')
+     return -1
+    else:
+     update.message.reply_text(text)
       
     
     
